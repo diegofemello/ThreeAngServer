879e3ad876a2ecd3f586d2f17917bee688a2c860
Diego Mello <dfmello@firjan.com.br>
2022-05-21 16:55:07 -0300

    Atualizando script de inicialização

--- src/index.js
+++ src/index.js
@@ -70,6 +70,6 @@ io.on("connection", (socket) => {
   console.log(`Socket ${socket.id} has connected`);
 });
 
-http.listen(4444, () => {
-  console.log("Listening on port 4444");
+http.listen(80, () => {
+  console.log("Listening on port 80");
 });
