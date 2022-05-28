0cc6e01448bb14f3c66ed9794722fa5d400e1807
Diego Mello <dfmello@firjan.com.br>
2022-05-27 21:45:14 -0300

    Atualizando e emitindo novo estilo

--- src/index.js
+++ src/index.js
@@ -51,11 +51,7 @@ io.on("connection", (socket) => {
     if (playerInArray) {
       playerInArray.position = player.position;
       playerInArray.rotation = player.rotation;
-      playerInArray.previousState = playerInArray.state;
       playerInArray.state = player.state;
-
-      socket.to(player.uid).emit("player", player);
-      socket.to(player.uid).emit("playersMovement", players);
     }
   });
 
@@ -65,8 +61,8 @@ io.on("connection", (socket) => {
     if (playerInArray) {
       playerInArray.style = player.style;
 
-      socket.to(player.uid).emit("player", player);
-      socket.to(player.uid).emit("playersMovement", players);
+      io.emit("playersMovement", players);
+      io.emit("styleUpdated", player.uid);
     }
   });
 
